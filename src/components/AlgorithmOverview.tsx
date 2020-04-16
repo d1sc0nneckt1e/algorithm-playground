52690cdfe9fb3ed29c23f0eefd6f103b1d990b5a
unknown <haehnchen.mike@gmail.com>
2020-04-16 17:00:01 +0200

    Add components/AlgorithmOverview.tsx

---
+++ src/components/AlgorithmOverview.tsx
@@ -0,0 +1,12 @@
+import React, { useRef, useState } from "react";
+import { IonApp } from "@ionic/react";
+
+const AlgorithmOverview: React.FC = () => {
+  return (
+    <IonApp>
+      <h2>AlgorithmOverview</h2>
+    </IonApp>
+  );
+};
+
+export default AlgorithmOverview;
