20af357df01cbb29eb5a8377a44e22ef6dd3e110
unknown <a.c.rezai@gmail.com>
2020-04-18 17:30:10 +0200

    -ADD modal

--- src/components/MainOverview.tsx
+++ src/components/MainOverview.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   IonContent,
   IonGrid,
@@ -6,11 +6,18 @@ import {
   IonCol,
   IonCard,
   IonCardHeader,
+  IonModal,
+  IonButton,
 } from "@ionic/react";
 
 import data from "./data";
+import { SSL_OP_ALLOW_UNSAFE_LEGACY_RENEGOTIATION } from "constants";
 
 const MainOverview: React.FC = () => {
+  const [showModal, setShowModal] = useState(false);
+  const modalStyling = {
+    color: "white",
+  };
   return (
     <IonContent>
       <IonGrid>
@@ -20,6 +27,19 @@ const MainOverview: React.FC = () => {
               <IonCard>
                 <IonCardHeader>{algo.title}</IonCardHeader>
               </IonCard>
+            </IonCol>
+          </IonRow>
+        ))}
+        {data.algorithms.map((algo, i) => (
+          <IonRow key={i}>
+            <IonCol>
+              <IonModal isOpen={showModal}>
+                <p style={modalStyling}>{algo.title}</p>
+                <IonButton onClick={() => setShowModal(false)}>
+                  Close Modal
+                </IonButton>
+              </IonModal>
+							<IonButton onClick={() => setShowModal(true)}>Show Modal</IonButton>
             </IonCol>
           </IonRow>
         ))}
